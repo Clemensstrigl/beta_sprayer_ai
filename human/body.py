356066112f843262f22bd514bd4d4bd8d96908c1
Clemensstrigl <clemens14.strigl@gmail.com>
2024-07-30 00:03:29 -0700

    fixed apeindex, fixing bones rn

--- human/body.py
+++ human/body.py
@@ -13,8 +13,9 @@ class Body():
         self.weight = weight
         self.canDoPullup = canDoPullup
         self.canDoPistleSquat = canDoPistleSquat
+        self.body_segments = self.def_segments()
         self.body_parts = self.create_body()
-        #self.skeleton = self.add_armature(self.body_parts)
+        self.skeleton = self.add_armature(self.body_parts)
 
 
     def create_pipe(self,name, radius, height, location, rotation=(0, 0, 0)):
@@ -28,49 +29,26 @@ class Body():
         sphere = bpy.context.object
         sphere.name = name
         return sphere
+    
+    def def_segments(self,):
+        ape_index_addon = self.apeindex/4
+        ape_index_center_offset = ape_index_addon/2
 
-    def create_body(self,):
-        body_parts = {}
-        
-       
 
-        body_segments = [
-            {"name": "head", "radius": 0.15, "location": (0, 0, 2.25)},
+        return [
+            {"name": "head", "radius": 0.0625*self.height, "location": (0, 0, 0.9375*self.height), "active": False},
             #{"name": "neck", "radius": 0.03, "height": 0.3, "location": (0, 0, 1.8)},
-            {"name": "chest", "radius": 0.24, "location": (0, 0, 1.85)},
-            {"name": "stomach", "radius": 0.18, "location": (0, 0, 1.43)},
-            {"name": "hips", "radius": 0.12, "height": 0.36, "location": (0, 0, 1.13),              "rotation": (0, 1.5708, 0)},
-            {"name": "collar_L", "radius": 0.09, "height": 0.24, "location": (-0.12, 0, 1.95),      "rotation": (0, 1.5708, 0)},
-            {"name": "collar_R", "radius": 0.09, "height": 0.24, "location": (0.12, 0, 1.95),       "rotation": (0, 1.5708, 0)},
-            {"name": "upper_arm_L", "radius": 0.08, "height": 0.45, "location": (-0.465, 0, 1.95),  "rotation": (0, 1.5708, 0)},
-            {"name": "upper_arm_R", "radius": 0.08, "height": 0.45, "location": (0.465, 0, 1.95),   "rotation": (0, 1.5708, 0)},
-            {"name": "lower_arm_L", "radius": 0.07, "height": 0.4, "location": (-0.89, 0, 1.95),    "rotation": (0, 1.5708, 0)},
-            {"name": "lower_arm_R", "radius": 0.07, "height": 0.4, "location": (0.89, 0, 1.95),     "rotation": (0, 1.5708, 0)},
-            {"name": "hand_L", "radius": 0.06,  "location": (-1.13, 0, 1.95),                       "rotation": (0, 1.5708, 0)},
-            {"name": "hand_R", "radius": 0.06,  "location": (1.13, 0, 1.95),                        "rotation": (0, 1.5708, 0)},
-            {"name": "upper_leg_L", "radius": 0.08, "height": 0.5, "location": (-0.1, 0, 0.78)},
-            {"name": "upper_leg_R", "radius": 0.08, "height": 0.5, "location": (0.1, 0, 0.78)},
-            {"name": "lower_leg_L", "radius": 0.07, "height": 0.4, "location": (-0.1, 0, 0.33)},
-            {"name": "lower_leg_R", "radius": 0.07, "height": 0.4, "location": (0.1, 0, 0.33)},
-            {"name": "foot_L", "radius": 0.08,  "location": (-0.1, 0, 0.08)},
-            {"name": "foot_R", "radius": 0.08,  "location": (0.1, 0, 0.08)},
-        ]
-
-
-        body_segments = [
-            {"name": "head", "radius": 0.0625*self.height, "location": (0, 0, 0.9375*self.height)},
-            #{"name": "neck", "radius": 0.03, "height": 0.3, "location": (0, 0, 1.8)},
-            {"name": "chest", "radius": 0.1*self.height, "location": (0, 0, 0.7708*self.height)},
+            {"name": "chest", "radius": 0.1*self.height, "location": (0, 0, 0.7708*self.height),"active": False},
             {"name": "stomach", "radius": 0.075*self.height, "location": (0, 0, 0.595*self.height)},
             {"name": "hips", "radius": 0.05*self.height, "height": 0.15*self.height, "location": (0, 0, 0.4708*self.height),              "rotation": (0, 1.5708, 0)},
-            {"name": "collar_L", "radius": 0.0375*self.height, "height": 0.1*self.height, "location": (-0.05*self.height, 0, 0.8125*self.height),      "rotation": (0, 1.5708, 0)},
-            {"name": "collar_R", "radius": 0.0375*self.height, "height": 0.1*self.height, "location": (0.05*self.height, 0, 0.8125*self.height),       "rotation": (0, 1.5708, 0)},
-            {"name": "upper_arm_L", "radius": 0.03*self.height, "height": 0.1875*self.height, "location": (-0.1938*self.height, 0, 0.8125*self.height),  "rotation": (0, 1.5708, 0)},
-            {"name": "upper_arm_R", "radius": 0.03*self.height, "height": 0.1875*self.height, "location": (0.1938*self.height, 0, 0.8125*self.height),   "rotation": (0, 1.5708, 0)},
-            {"name": "lower_arm_L", "radius": 0.029*self.height, "height": 0.1667*self.height, "location": (-0.3708*self.height, 0, 0.8125*self.height),    "rotation": (0, 1.5708, 0)},
-            {"name": "lower_arm_R", "radius": 0.029*self.height, "height": 0.1667*self.height, "location": (0.3708*self.height, 0, 0.8125*self.height),     "rotation": (0, 1.5708, 0)},
-            {"name": "hand_L", "radius": 0.03*self.height,  "location": (-0.4708*self.height, 0, 0.8125*self.height),                       "rotation": (0, 1.5708, 0)},
-            {"name": "hand_R", "radius": 0.03*self.height,  "location": (0.4708*self.height, 0, 0.8125*self.height),                        "rotation": (0, 1.5708, 0)},
+            {"name": "collar", "radius": 0.0375*self.height, "height": 0.2*self.height, "location": (0, 0, 0.8125*self.height),      "rotation": (0, 1.5708, 0)},
+            #{"name": "collar_R", "radius": 0.0375*self.height, "height": 0.1*self.height, "location": (0.05*self.height, 0, 0.8125*self.height),       "rotation": (0, 1.5708, 0)},
+            {"name": "upper_arm_L", "radius": 0.03*self.height, "height": 0.1875*self.height + ape_index_addon, "location": (-0.1938*self.height - ape_index_center_offset, 0, 0.8125*self.height),  "rotation": (0, 1.5708, 0)},
+            {"name": "upper_arm_R", "radius": 0.03*self.height, "height": 0.1875*self.height + ape_index_addon, "location": (0.1938*self.height+ ape_index_center_offset, 0, 0.8125*self.height),   "rotation": (0, 1.5708, 0)},
+            {"name": "lower_arm_L", "radius": 0.029*self.height, "height": 0.1667*self.height + ape_index_addon, "location": (-0.3708*self.height- ape_index_addon -ape_index_center_offset, 0, 0.8125*self.height),    "rotation": (0, 1.5708, 0)},
+            {"name": "lower_arm_R", "radius": 0.029*self.height, "height": 0.1667*self.height + ape_index_addon, "location": (0.3708*self.height+ ape_index_addon + ape_index_center_offset, 0, 0.8125*self.height),     "rotation": (0, 1.5708, 0)},
+            {"name": "hand_L", "radius": 0.03*self.height,  "location": (-0.4708*self.height- ape_index_addon*2, 0, 0.8125*self.height),                       "rotation": (0, 1.5708, 0)},
+            {"name": "hand_R", "radius": 0.03*self.height,  "location": (0.4708*self.height+ ape_index_addon*2, 0, 0.8125*self.height),                        "rotation": (0, 1.5708, 0)},
             {"name": "upper_leg_L", "radius": 0.03*self.height, "height": 0.2083*self.height, "location": (-0.0417*self.height, 0, 0.325*self.height)},
             {"name": "upper_leg_R", "radius": 0.03*self.height, "height": 0.2083*self.height, "location": (0.0417*self.height, 0, 0.325*self.height)},
             {"name": "lower_leg_L", "radius": 0.029*self.height, "height": 0.1667*self.height, "location": (-0.0417*self.height, 0, 0.1375*self.height)},
@@ -78,12 +56,27 @@ class Body():
             {"name": "foot_L", "radius": 0.03*self.height,  "location": (-0.0417*self.height, 0, 0.03*self.height)},
             {"name": "foot_R", "radius": 0.03*self.height,  "location": (0.0417*self.height, 0, 0.03*self.height)},
         ]
+    
+    def get_segment_attr(self, segment, attr):
+        for s in self.body_segments:
+            if segment in s["name"]:
+                if attr in s.keys():
+                    return s[attr]
+                elif attr == "height":
+                    return s["radius"]*2
+                return None
+
+    def create_body(self,):
+        body_parts = {}
+        
+       
+
         
         
 
         
         # Create pipes for each body segment
-        for segment in body_segments:
+        for segment in self.body_segments:
             rotation = segment.get("rotation", (0, 0, 0))
             if "height" in segment:
                 body_parts[segment["name"]] = self.create_pipe(segment["name"], segment["radius"], segment["height"], segment["location"], rotation)
@@ -92,9 +85,36 @@ class Body():
 
             
         return body_parts
+    
+    def get_obj_loc(self, obj_name):
+        if "collar" in obj_name:
+            if "L" in obj_name:
+                loc = list(self.get_segment_attr("collar", "location"))
+                loc[0] = loc[0] - self.get_segment_attr("collar", "height")/2
+                return tuple(loc)
+            if "R" in obj_name:
+                loc = list(self.get_segment_attr("collar", "location"))
+                loc[0] = loc[0] + self.get_segment_attr("collar", "height")/2
+                return tuple(loc)
+            return self.get_segment_attr("collar", "location")
+        if "hips" in obj_name:
+            if "L" in obj_name:
+                loc = list(self.get_segment_attr("hips", "location"))
+                loc[0] = list(self.get_segment_attr("lower_leg_L", "location"))[0]
+                return tuple(loc)
+            if "R" in obj_name:
+                loc = list(self.get_segment_attr("hips", "location"))
+                loc[0] = list(self.get_segment_attr("lower_leg_R", "location"))[0]
+                return tuple(loc)
+            return self.get_segment_attr("hips", "location")
+
+        return self.get_segment_attr(obj_name, "location")
+        
+            
+
         
     def add_armature(self, body_parts):
-        bpy.ops.object.armature_add(location=(0, 0, 0))
+        bpy.ops.object.armature_add()
         armature = bpy.context.object
         armature.name = 'Armature'
         
@@ -105,56 +125,119 @@ class Body():
         
         # Define bones and their connections
         bones_data = [
-            #("hips", None),
-            ("stomach", "hips"),
+            #("chest", None),
+            
             ("chest", "stomach"),
-            ("head", "chest"),
-            ("collar_L", "chest"),
-            ("collar_R", "chest"),
+            ("stomach", "hips"),
+            ("collar", "chest"),
+            ("head", "collar"),
+
+            ("collar_L", "collar"),
+            ("collar_R", "collar"),
+            ("collar", "chest"),
             ("upper_arm_L", "collar_L"),
-            ("upper_arm_R", "collar_R"),
+            ("upper_arm_R", "collar_L"),
             ("lower_arm_L", "upper_arm_L"),
             ("lower_arm_R", "upper_arm_R"),
             ("hand_L", "lower_arm_L"),
             ("hand_R", "lower_arm_R"),
-            ("upper_leg_L", "hips"),
-            ("upper_leg_R", "hips"),
+            ("hips_L", "hips"),
+            ("hips_R", "hips"),
+            ("upper_leg_L", "hips_L"),
+            ("upper_leg_R", "hips_R"),
             ("lower_leg_L", "upper_leg_L"),
             ("lower_leg_R", "upper_leg_R"),
             ("foot_L", "lower_leg_L"),
             ("foot_R", "lower_leg_R"),
         ]
+
+
+
+
+
+
+
+
+
+
         
         for bone_name, parent_name in bones_data:
             bone = edit_bones.new(bone_name)
-            bone.head = body_parts[bone_name].location
+            parent_loc = list(self.get_obj_loc(parent_name))
+            child_loc = list(self.get_obj_loc(bone_name))
+            bone_length = self.get_segment_attr(bone_name, "height")
+
+
+            if "collar" in parent_name:
+                if "collar_L" in bone_name:
+                    child_loc[0] = body_parts[parent_name].location[0] - self.get_segment_attr(parent_name, "height")/2
+                    bone_length = self.get_segment_attr(parent_name, "height")/2
+
+                elif "collar_R" in bone_name:
+                    child_loc[0] = body_parts[parent_name].location[0] + self.get_segment_attr(parent_name, "height")/2
+                    bone_length = self.get_segment_attr(parent_name, "height")/2
+
+                elif "arm_L" in bone_name:
+                    parent_loc[0] = body_parts["collar"].location[0] - self.get_segment_attr("collar", "height")/2
+                    child_loc[0] = body_parts[bone_name].location[0] - self.get_segment_attr(bone_name, "height")/2
+
+                elif "arm_R" in bone_name:
+                    parent_loc[0] = body_parts["collar"].location[0] + self.get_segment_attr("collar", "height")/2
+                    child_loc[0] = body_parts[bone_name].location[0] + self.get_segment_attr(bone_name, "height")/2
+
             
-            bone_length = body_parts[bone_name].dimensions[2]
-            # Set bone length based on body part dimensions
-            if "arm" in bone_name: # or "leg" in bone_name:
-                bone_length = body_parts[bone_name].dimensions[2]  # height of the body part
-            elif "leg" in bone_name:
-                bone_length = body_parts[bone_name].dimensions[2]  # height of the body part
-            else:
-                bone_length = body_parts[bone_name].dimensions[2]  # height of the body part
+
+                    
+            elif "hips" in parent_name:
+                if "hips_L" in bone_name:
+
+                    child_loc[0] = self.get_segment_attr("upper_leg_L","location")[0]
+                    bone_length = self.get_segment_attr(parent_name, "height")/2 + self.get_segment_attr("upper_leg_L", "radius")/2
+
+                elif "hips_R" in bone_name:
+                    child_loc[0] = self.get_segment_attr("upper_leg_R","location")[0]
+                    bone_length = self.get_segment_attr(parent_name, "height")/2 + self.get_segment_attr("upper_leg_L", "radius")/2
+                
+                elif "leg_L" in bone_name:
+                    print(bone_name)
+                    child_loc[2] = child_loc[2] - self.get_segment_attr(bone_name, "height")/2
+
+                elif "leg_R" in bone_name:
+                    child_loc[2] = child_loc[2] - self.get_segment_attr(bone_name, "height")/2
+
+
+            elif 'leg' in parent_name:
+                child_loc = body_parts[bone_name].location
+                parent_loc[2] = parent_loc[2] - self.get_segment_attr(parent_name, "height")/2
+                child_loc[2] = child_loc[2] - self.get_segment_attr(bone_name, "height")/2
+
+            elif 'arm' in parent_name:
+                child_loc = body_parts[bone_name].location
+                parent_loc[0] = parent_loc[0] - self.get_segment_attr(parent_name, "height")/2
+                child_loc[0] = child_loc[0] - self.get_segment_attr(bone_name, "height")/2
+
+            print(bone_name)
+            bone.length = bone_length
+            bone.head = tuple(parent_loc)
+            if child_loc:
+                bone.tail = tuple(child_loc)
             
-            if "L" in bone_name:
-                bone.tail = (bone.head[0] - bone_length, bone.head[1], bone.head[2])
-            elif "R" in bone_name:
-                bone.tail = (bone.head[0] + bone_length, bone.head[1], bone.head[2])
-            else:
-                bone.tail = (bone.head[0], bone.head[1], bone.head[2] + bone_length)
+         
             
-            if parent_name:
+            if parent_name in edit_bones:
                 bone.parent = edit_bones[parent_name]
+            
+        
+
         
         bpy.ops.object.mode_set(mode='OBJECT')
         
         # Parent each body part to its corresponding bone and hide the body parts
         for bone_name, body_part in body_parts.items():
+            print(bone_name, body_part.name)
             bpy.context.view_layer.objects.active = body_part
             bpy.ops.object.parent_set(type='BONE', keep_transform=True)
-            body_part.hide_viewport = True  # Hide body parts
+            bpy.ops.object.hide_viewport = True  # Hide body parts
         
         return armature
 
@@ -194,7 +277,7 @@ def main():
     bpy.ops.object.select_by_type(type='MESH')
     bpy.ops.object.delete()
     
-    body = Body(False, 2.4, 5, 86, canDoPullup=True, canDoPistleSquat=True)
+    body = Body(False, 1.86, 0., 86, canDoPullup=True, canDoPistleSquat=True)
     
     
     # Example usage
